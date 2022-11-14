bcd6d15cdd88ef3e08244a073165432d6ed2e46f
Luca <62838131+xZeenos@users.noreply.github.com>
2022-11-14 11:45:40 +0100

    Update star_schema.sql

--- star_schema.sql
+++ star_schema.sql
@@ -1,5 +1,10 @@
-SELECT SUM(kredit.kreditvolumen) AS Kreditvolumen, SUM(rueckzahlung.tilgungsanteil) + SUM(rueckzahlung.zinsanteil) AS Umsatz 
-FROM kredit
-INNER JOIN rueckzahlung
-ON rueckzahlung.kreditid = kredit.kreditid
-
+SELECT SUM(k.kreditvolumen) AS Kreditvolumen, SUM(r.tilgungsanteil) + SUM(r.zinsanteil) AS Umsatz 
+FROM kredit k
+INNER JOIN rueckzahlung r
+ON r.kreditid = k.kreditid
+INNER JOIN kreditverantwortlicher kv
+ON kv.kreditid = k.kreditid
+INNER JOIN vertriebspartner v
+ON kv.vertriebspartnerid = v.vertriebspartnerid
+INNER JOIN provisionsvertrag p
+ON v.provisionsvertragid = p.provisionsvertragid
