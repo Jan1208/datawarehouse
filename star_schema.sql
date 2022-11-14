32fe37ab541d10f360fedaa80be0b3cda1796a1b
Jan H <mail@huebnerjan.de>
2022-11-14 13:05:56 +0100

    Update star_schema.sql

--- star_schema.sql
+++ star_schema.sql
@@ -8,3 +8,22 @@ LEFT JOIN vertriebspartner v
 ON kv.vertriebspartnerid = v.vertriebspartnerid
 LEFT JOIN provisionsvertrag p
 ON v.provisionsvertragid = p.provisionsvertragid
+
+
+
+
+
+
+CREATE VIEW kredit_provision AS
+  SELECT k.kreditid, pv.hoehe as Provisionshoehe
+  FROM kredit k
+  LEFT JOIN kreditverantwortlicher kv
+  ON kv.kreditid = k.kreditid
+  LEFT JOIN vertriebspartner vp
+  ON vp.vertriebspartnerid = kv.vertriebspartnerid
+  LEFT JOIN provisionsvertrag pv
+  ON pv.provisionsvertragid = vp.provisionsvertragid
+  
+CREATE VIEW kredit_gesamtRueckzahlung AS 
+	SELECT SUM(tilgungsanteil) AS tilgungen, SUM(zinsanteil) as zinsen, SUM(tilgungsanteil) + SUM(zinsanteil) AS Gesamt
+	FROM rueckzahlung
