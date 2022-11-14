f4a2d12051b7d80082d92fd9aff978e415dcc4c4
Luca <62838131+xZeenos@users.noreply.github.com>
2022-11-14 11:36:48 +0100

    Update star_schema.sql

--- star_schema.sql
+++ star_schema.sql
@@ -1,1 +1,5 @@
+SELECT SUM(kredit.kreditvolumen) AS Kreditvolumen, SUM(rueckzahlung.tilgungsanteil) + SUM(rueckzahlung.zinsanteil) AS Umsatz 
+FROM kredit
+INNER JOIN rueckzahlung
+ON rueckzahlung.kreditid = kredit.kreditid
 
