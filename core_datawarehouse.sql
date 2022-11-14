ae6d6bc09f81224d819e0aa821b72500851001a3
Jan H <mail@huebnerjan.de>
2022-11-14 10:22:10 +0100

    Update core_datawarehouse.sql

--- core_datawarehouse.sql
+++ core_datawarehouse.sql
@@ -1,67 +1,67 @@
 CREATE TABLE Kredit (
-  KreditID SERIAL PRIMARY KEY, 
-  Zinsen float,
-  Kreditvolumen float,
-  Abschlusskosten float,
-  Geforderte_garantie float,
-  Personalkosten float,
-  Anfangstermin DATE,
-  Endtermin DATE,
-  KundenID INT,
-  KreditartID INT,
-  gueltig_ab TIMESTAMP,
-  gueltig_bis TIMESTAMP
+	KreditID SERIAL PRIMARY KEY, 
+	Zinsen float,
+	Kreditvolumen float,
+	Abschlusskosten float,
+	Geforderte_garantie float,
+	Personalkosten float,
+	Anfangstermin DATE,
+	Endtermin DATE,
+	KundenID INT,
+	KreditartID INT,
+	gueltig_ab TIMESTAMP,
+	gueltig_bis TIMESTAMP
 );
 
 CREATE TABLE Kreditart (
-  KreditID INT NOT NULL,
-  Kreditart VARCHAR(50),
-  gueltig_ab TIMESTAMP,
-  gueltig_bis TIMESTAMP
+	KreditID INT NOT NULL,
+	Kreditart VARCHAR(50),
+	gueltig_ab TIMESTAMP,
+	gueltig_bis TIMESTAMP
 );
 
 CREATE TABLE Kunde (
-  KundenID SERIAL PRIMARY KEY,
-  Vorname VARCHAR(50),
-  Nachname VARCHAR(50),
-  Geburtstag DATE,
-  Nettoeinkommen_monat float,
-  AdressID INT,
-  BerufID INT,
-  KundentypID INT,
-  gueltig_ab TIMESTAMP,
-  gueltig_bis TIMESTAMP
+	KundenID SERIAL PRIMARY KEY,
+	Vorname VARCHAR(50),
+	Nachname VARCHAR(50),
+	Geburtstag DATE,
+	Nettoeinkommen_monat float,
+	AdressID INT,
+	BerufID INT,
+	KundentypID INT,
+	gueltig_ab TIMESTAMP,
+	gueltig_bis TIMESTAMP
 );
 
 CREATE TABLE Beruf (
-  BerufID SERIAL PRIMARY KEY,
-  Berufname varchar(50),
-  gueltig_ab TIMESTAMP,
-  gueltig_bis TIMESTAMP
+	BerufID SERIAL PRIMARY KEY,
+	Berufname varchar(50),
+	gueltig_ab TIMESTAMP,
+	gueltig_bis TIMESTAMP
 );
 
 CREATE TABLE Mitarbeiter (
-  MitarbeiterID SERIAL PRIMARY KEY,
-  Vorname VARCHAR NOT NULL,
-  Nachname VARCHAR NOT NULL,
-  AdressID INT NOT NULL,
-  KreditID INT NOT NULL,
-  Alter INT,
-  Gehalt float,
-  Beruf VARCHAR(50),
-  Gueltig_ab TIMESTAMP,
-  Gueltig_bis TIMESTAMP
+	MitarbeiterID SERIAL PRIMARY KEY,
+	Vorname VARCHAR NOT NULL,
+	Nachname VARCHAR NOT NULL,
+	AdressID INT NOT NULL,
+	KreditID INT NOT NULL,
+	Alter INT,
+	Gehalt float,
+	Beruf VARCHAR(50),
+	Gueltig_ab TIMESTAMP,
+	Gueltig_bis TIMESTAMP
 );
 
 CREATE TABLE Adresse (
-  AdressID SERIAL PRIMARY KEY,
-  Hausnummer INT,
-  Straße VARCHAR (50),
-  Stadt VARCHAR (50),
-  PLZ INT,
-  Land VARCHAR (50),
-  gueltig_ab TIMESTAMP,
-  gueltig_bis TIMESTAMP
+	AdressID SERIAL PRIMARY KEY,
+	Hausnummer INT,
+	Straße VARCHAR (50),
+	Stadt VARCHAR (50),
+	PLZ INT,
+	Land VARCHAR (50),
+	gueltig_ab TIMESTAMP,
+	gueltig_bis TIMESTAMP
 );
 
 CREATE TABLE Partnerart (
@@ -83,24 +83,24 @@ CREATE TABLE Vertriebspartner (
 );
 
 CREATE TABLE Kreditverantwortlicher (
-  KreditID INTEGER NOT NULL PRIMARY KEY REFERENCES Kredit(KreditID),
-  MitarbeiterID INTEGER NOT NULL PRIMARY KEY REFERENCES Mitarbeiter(MitarbeiterID),
-  VertriebspartnerID INTEGER PRIMARY KEY REFERENCES Vertriebspartner(VPartnerID),
+	KreditID INTEGER NOT NULL PRIMARY KEY REFERENCES Kredit(KreditID),
+	MitarbeiterID INTEGER NOT NULL PRIMARY KEY REFERENCES Mitarbeiter(MitarbeiterID),
+	VertriebspartnerID INTEGER PRIMARY KEY REFERENCES Vertriebspartner(VPartnerID)
 );
 
 CREATE TABLE Person (
-  RueckzahlungsID SERIAL NOT NULL PRIMARY KEY,
-  KreditID INTEGER NOT NULL REFERENCES Kredit(KreditID),
-  Tilgungsanteil FLOAT NOT NULL,
-  Zisnanteil FLOAT NOT NULL,
-  Datum DATE NOT NULL,
-  gueltig_ab TIMESTAMP NOT NULL,
-  gueltig_bis TIMESTAMP NOT NULL,
+	RueckzahlungsID SERIAL NOT NULL PRIMARY KEY,
+	KreditID INTEGER NOT NULL REFERENCES Kredit(KreditID),
+	Tilgungsanteil FLOAT NOT NULL,
+	Zisnanteil FLOAT NOT NULL,
+	Datum DATE NOT NULL,
+	gueltig_ab TIMESTAMP NOT NULL,
+	gueltig_bis TIMESTAMP NOT NULL
 );
 
 CREATE TABLE buergt (
-  KundenID INTEGER NOT NULL PRIMARY KEY REFERENCES Kunde(KundenID),
-  BuergenID INTEGER NOT NULL PRIMARY KEY REFERENCES Buergen(BuergenID)
+	KundenID INTEGER NOT NULL PRIMARY KEY REFERENCES Kunde(KundenID),
+	BuergenID INTEGER NOT NULL PRIMARY KEY REFERENCES Buergen(BuergenID)
 );
 
 CREATE TABLE Kundentyp (
