a79d4cb6e4c1079a1b6b3731bfd7ae5ff24d3a0e
Jan H <mail@huebnerjan.de>
2022-11-24 14:36:38 +0100

    Update and rename snowflake_schema to snowflake_schema.sql

---
+++ snowflake_schema.sql
@@ -0,0 +1,19 @@
+
+// DIMENSIONS-TABELLEN
+CREATE VIEW snow_vertriebspartner AS
+	SELECT vertriebspartnerid, partnerartid FROM vertriebspartner;
+  
+ CREATE VIEW snow_partnerart AS
+	SELECT partnerartid, partnerart FROM partnerart;
+  
+CREATE VIEW snow_kunde AS 
+	SELECT kundenid, kundentypid FROM kunde;
+
+CREATE VIEW snow_kundentyp AS
+	SELECT kundentypid, kundentyp FROM kundentyp;
+  
+CREATE VIEW snow_plz AS
+	SELECT plzid, plz, regionname landid FROM plz
+ 
+ CREATE VIEW snow_land AS 
+	SELECT landid, land FROM land
